98ecf3003210dd74d24650ad630943312394014a
Kirillog <kirill.mitkin.03@mail.ru>
2021-10-12 22:42:08 +0300

    introduce createWindowOf function and Renderer of window layer

---
+++ src/main/kotlin/Window.kt
@@ -0,0 +1,51 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.swing.Swing
+import org.jetbrains.skija.*
+import org.jetbrains.skiko.SkiaLayer
+import org.jetbrains.skiko.SkiaRenderer
+import org.jetbrains.skiko.SkiaWindow
+import java.awt.Dimension
+import javax.swing.WindowConstants
+
+fun createWindowOf(chart: Chart) = runBlocking(Dispatchers.Swing) {
+    val window = SkiaWindow()
+    window.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
+    window.title = chart.name
+
+    window.layer.renderer = Renderer(window.layer) {
+        renderer, width, height -> drawChart(renderer, width, height, chart)
+    }
+
+    window.preferredSize = Dimension(800, 600)
+    window.minimumSize = Dimension(100, 100)
+    window.pack()
+    window.layer.awaitRedraw()
+    window.isVisible = true
+}
+
+class Renderer(
+    val layer: SkiaLayer,
+    val drawChart: (Renderer, Int, Int) -> Unit
+) : SkiaRenderer {
+    val typeface = Typeface.makeFromFile("fonts/JetBrainsMono-Regular.ttf")
+    var startHeight = 0
+    var canvas: Canvas? = null
+    val font = Font(typeface, 40f)
+    val paint = Paint().apply {
+        mode = PaintMode.STROKE
+        strokeWidth = 1f
+    }
+
+    val fillPaint = Paint().apply {
+        mode = PaintMode.FILL
+    }
+
+    override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
+        this.canvas = canvas
+        val contentScale = layer.contentScale
+        canvas.scale(contentScale, contentScale)
+        drawChart(this, (width / contentScale).toInt(), (height / contentScale).toInt())
+        layer.needRedraw()
+    }
+}
