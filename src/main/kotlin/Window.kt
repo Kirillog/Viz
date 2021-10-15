67e2d96f60104914f98f8cb3ccb463c1e614da28
Kirillog <kirill.mitkin.03@mail.ru>
2021-10-15 12:41:07 +0300

    add saving to file

--- src/main/kotlin/Window.kt
+++ src/main/kotlin/Window.kt
@@ -1,28 +1,91 @@
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.swing.Swing
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jetbrains.skija.*
 import org.jetbrains.skiko.SkiaLayer
 import org.jetbrains.skiko.SkiaRenderer
-import org.jetbrains.skiko.SkiaWindow
+import org.jetbrains.skiko.toBufferedImage
 import java.awt.Dimension
-import javax.swing.WindowConstants
+import java.awt.Toolkit
+import java.awt.event.*
+import java.io.File
+import javax.imageio.ImageIO
+import javax.swing.*
+import javax.swing.filechooser.FileFilter
 import kotlin.math.min
 
-fun createWindowOf(chart: Chart) = runBlocking(Dispatchers.Swing) {
-    val window = SkiaWindow()
+class ChartFileFilter(var extension: String?, private var description: String?) : FileFilter() {
+
+    override fun accept(file: File?): Boolean {
+        if (file != null) {
+            return if (file.isDirectory)
+                true
+            else if (extension == null)
+                extension!!.isEmpty()
+            else
+                file.name.endsWith(extension!!)
+        }
+        return false
+    }
+
+    override fun getDescription(): String? {
+        return description
+    }
+}
+
+@DelicateCoroutinesApi
+fun createMenu(window: JFrame, panel: SkiaPanel) {
+    val menuBar = JMenuBar()
+    val fileMenu = JMenu("Chart")
+    menuBar.add(fileMenu)
+
+    val fileChooser = JFileChooser()
+
+    val miTakeScreenshot = JMenuItem("Screenshot")
+    val ctrlS = KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().menuShortcutKeyMaskEx)
+    miTakeScreenshot.accelerator = ctrlS
+    miTakeScreenshot.addActionListener {
+        fileChooser.dialogType = JFileChooser.SAVE_DIALOG
+        fileChooser.fileSelectionMode = JFileChooser.FILES_ONLY
+        val fileFilter = ChartFileFilter("png", "Charts (*.png)")
+        fileChooser.addChoosableFileFilter(fileFilter)
+        val result = fileChooser.showSaveDialog(panel)
+        if (result == JFileChooser.APPROVE_OPTION) {
+            val selectedFile = if (fileFilter.accept(fileChooser.selectedFile))
+                fileChooser.selectedFile
+            else
+                File("${fileChooser.selectedFile.absolutePath}.${fileFilter.extension}")
+            val screenshot = panel.layer.screenshot()!!
+            GlobalScope.launch(Dispatchers.IO) {
+                val image = screenshot.toBufferedImage()
+                ImageIO.write(image, "png", selectedFile)
+                println("Saved to ${selectedFile.absolutePath}")
+            }
+        }
+    }
+    fileMenu.add(miTakeScreenshot)
+    window.jMenuBar = menuBar
+}
+
+@DelicateCoroutinesApi
+fun createWindowOf(chart: Chart) = SwingUtilities.invokeLater {
+    val window = JFrame()
     window.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
     window.title = chart.name
 
-    window.layer.renderer = Renderer(window.layer) {
-        renderer, width, height -> drawChart(renderer, width, height, chart)
+    val panel = SkiaPanel()
+    panel.layer.renderer = Renderer(panel.layer) { renderer, width, height ->
+        drawChart(renderer, width, height, chart)
     }
+    window.contentPane.add(panel)
 
-    window.preferredSize = Dimension(800, 600)
-    window.minimumSize = Dimension(100, 100)
+    createMenu(window, panel)
+
     window.pack()
-    window.layer.awaitRedraw()
     window.isVisible = true
+    window.size = Dimension(800, 600)
+    window.minimumSize = Dimension(200, 200)
 }
 
 class Renderer(
@@ -49,11 +112,11 @@ class Renderer(
         mode = PaintMode.FILL
     }
 
-    fun fontAt(height : Int, width: Int, textLength:Int): Font {
+    fun fontAt(height: Int, width: Int, textLength: Int): Font {
         return Font(typeface, calculateFontSize(height, width, textLength))
     }
 
-    fun calculateFontSize(height : Int, width: Int, textLength:Int) : Float {
+    private fun calculateFontSize(height: Int, width: Int, textLength: Int): Float {
         return min(font.size, min(height.toFloat(), width.toFloat() / textLength))
     }
 
